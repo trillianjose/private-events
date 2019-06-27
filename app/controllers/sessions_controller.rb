633d3bb7c01299db4e1466012e0ec4a0e2aa6f1b
Shshamim63 <shossain142056@bscse.uiu.ac.bd>
2019-06-27 17:56:45 +0600

    added controller,model and view

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+
+  def create
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:email])
+      log_in @user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      redirect_to :root
+    else
+      flash.now[:danger] = 'Invalid email'
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out if logged_in?
+    redirect_to :root
+  end
+
+end
