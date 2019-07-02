b7ad3f2898384980114f1e5c5329a527d4a29e20
trillianjose <trillianjosea@gmail.com>
2019-07-02 17:09:07 +0200

    fixing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Rails.application.routes.draw do
   root 'events#index'
 
-  get       '/signup',   to: 'users#new'
-  post      '/signup',   to: 'users#create'
-  get       '/login',    to: 'sessions#new'
-  post      '/login',    to: 'sessions#create'
-  delete    '/logout',   to: 'sessions#destroy'
-  resources 'users',     only: [:new, :create, :show]
-  resources 'sessions',  only: [:new]
-  resources 'events',    only: [:index, :new, :create]
-  resources 'attendance' do
-    post :checkin
-    get  :assistents
+  get       '/signup',       to: 'users#new'
+  post      '/signup',       to: 'users#create'
+  get       '/login',        to: 'sessions#new'
+  post      '/login',        to: 'sessions#create'
+  delete    '/logout',       to: 'sessions#destroy'
+  resources 'users',         only: [:new, :create, :show]
+  resources 'sessions',      only: [:new]
+  resources 'events',        only: [:index, :new, :create] do
+    resources 'attendances', only: [:index, :create]
   end
 end
